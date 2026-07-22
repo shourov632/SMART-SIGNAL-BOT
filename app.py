e17ca9baed816915e1f2bf0c0b6e9cb34b211a2b
shourov632 <zoomx472@gmail.com>
2026-07-22 13:15:03 +0600

    Set up Streamlit app for Forex Signal Scanner
    Initialize Streamlit app with title and time display.

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import streamlit as st
+from tradingview_ta import TA_Handler, Interval
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+st.set_page_config(
+    page_title="SMART SIGNAL BOT V11",
+    page_icon="📈",
+    layout="wide"
+)
+
+st.title("🤖 SMART SIGNAL BOT V11 PRO")
+st.write("Forex Live Signal Scanner")
+
+bd_time = datetime.now(ZoneInfo("Asia/Dhaka"))
+
+st.success(
+    "🇧🇩 Bangladesh Time : " +
+    bd_time.strftime("%d-%m-%Y %I:%M:%S %p")
+)
+
+pairs = [
+    "EURUSD",
+    "GBPUSD",
+    "USDJPY",
+    "AUDUSD",
+    "USDCAD",
+    "USDCHF",
+    "NZDUSD"
+]
